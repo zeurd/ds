2bd1f96f8011c6df6a21734b16a277bba353f0c4
Thomas <tdruez@icloud.com>
2021-01-13 13:47:59 +0100

    bitmask

--- combinations.go
+++ combinations.go
@@ -4,7 +4,9 @@ import "fmt"
 
 // Combinations foo
 type Combinations struct {
+	n            int
 	combinations [][]int
+	bitmasks     [][]uint8
 }
 
 // Size returns the number of all possible combinations
@@ -15,7 +17,9 @@ func (c *Combinations) Size() int {
 // NewCombinations foo
 func NewCombinations(n, r int) *Combinations {
 	c := &Combinations{
+		n,
 		make([][]int, 0, n),
+		make([][]uint8, 0, n),
 	}
 	data := make([]int, r)
 	c.combine(data, 0, n-1, 0)
@@ -29,7 +33,12 @@ func (c *Combinations) add(data []int) {
 func (c *Combinations) combine(data []int, start, end, i int) {
 	if i == len(data) {
 		d := make([]int, len(data))
+		bitmask := make([]uint8, c.n)
 		copy(d, data)
+		for _, x := range d {
+			bitmask[x] = 1
+		}
+		c.bitmasks = append(c.bitmasks, bitmask)
 		c.add(d)
 		return
 	}
@@ -47,4 +56,9 @@ func (c *Combinations) String() string {
 // All returns all the combinatins
 func (c *Combinations) All() [][]int {
 	return c.combinations
+}
+
+// Bitmasks foo
+func (c *Combinations) Bitmasks() [][]uint8 {
+	return c.bitmasks
 }
